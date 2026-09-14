7d00b229a99a22f9cb5e23972bf6c12bcf950583
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-09-14 10:55:19 -0300

    docs: streamline supported formats section
    Refer to the Format enum instead of keeping a manually maintained list
    of each format.

--- any2nix/src/lib.rs
+++ any2nix/src/lib.rs
@@ -24,10 +24,7 @@
 //!
 //! # Supported Formats
 //!
-//! - [INI](https://en.wikipedia.org/wiki/INI_file)
-//! - [JSON](https://www.json.org)
-//! - [TOML](https://toml.io)
-//! - [YAML](https://yaml.org)
+//! See [Format] for an enumeration of supported formats.
 //!
 //! Each format is gated behind its own [feature](https://doc.rust-lang.org/cargo/reference/features.html).
 //!
