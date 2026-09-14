e45ca86923b37931907b75f1021f0f03920ddf23
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-09-13 22:09:37 -0300

    style: try to be idiomatic with some use statements

--- any2nix-api/src/responses.rs
+++ any2nix-api/src/responses.rs
@@ -3,11 +3,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use serde::Serialize;
+#[cfg(feature = "utoipa")]
+use utoipa::ToSchema;
 
 /// Response structure for a successful conversion. Includes only one "nix"
 /// key.
 #[derive(Serialize)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[cfg_attr(feature = "utoipa", derive(ToSchema))]
 pub struct ConvertResponse {
     /// Houses the converted data.
     pub nix: String,
@@ -15,7 +17,7 @@ pub struct ConvertResponse {
 
 /// Response structure for an unsuccessful conversion. Includes only one "error" key.
 #[derive(Serialize)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[cfg_attr(feature = "utoipa", derive(ToSchema))]
 pub struct ErrorResponse {
     /// Houses the error message of the error.
     pub error: String,
@@ -23,7 +25,7 @@ pub struct ErrorResponse {
 
 /// Response structure for the root endpoint. Inclues crate information.
 #[derive(Serialize)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[cfg_attr(feature = "utoipa", derive(ToSchema))]
 pub struct RootResponse {
     pub description: String,
     #[cfg(feature = "utoipa-swagger-ui")]
