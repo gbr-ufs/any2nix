2539b4eb2953cae63e9f585386e38b6348f744d4
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-09-14 23:51:37 -0300

    feat(highlighting): add line numbers and rainbow delimiters to output

--- any2nix-website/assets/js/index.js
+++ any2nix-website/assets/js/index.js
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import "/node_modules/htmx.org/dist/htmx.min.js";
 import "/node_modules/prismjs/prism.js";
 import "/node_modules/prismjs/components/prism-ini.min.js";
 import "/node_modules/prismjs/components/prism-json.min.js";
@@ -12,7 +13,8 @@ import "/node_modules/prismjs/components/prism-yaml.min.js";
 import "/node_modules/prismjs/components/prism-nix.min.js";
 import "/node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js";
 import "/node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
+import "/node_modules/prismjs/plugins/line-numbers/prism-line-numbers.min.js";
+import "/node_modules/prismjs/plugins/match-braces/prism-match-braces.min.js";
 import "/node_modules/beercss/dist/cdn/beer.min.js";
-import "/node_modules/htmx.org/dist/htmx.min.js";
 import "./htmx.js";
 import "./editor.js";
