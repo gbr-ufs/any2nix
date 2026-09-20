2be5c2cdc1cd6137bc7e5c989bad48673a1cdc8c
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-09-20 03:38:44 -0300

    fix(lib.rs): remove unecessary braces from use statement

--- any2nix-website/src/lib.rs
+++ any2nix-website/src/lib.rs
@@ -135,7 +135,7 @@ pub fn app() -> Router {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use any2nix_i18n::{EN_US};
+    use any2nix_i18n::EN_US;
 
     #[tokio::test]
     async fn returns_index_page() {
